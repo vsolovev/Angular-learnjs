f8c0a8d96bc2f2f3fa4487253860428eb0d588dd
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-09-26 22:07:21 +0300

    add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import test from '@test';
+
+window.user;
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+    user: { name: string };
+}
+
+declare module '@test';
